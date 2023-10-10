6fa4bbee35afde646ac259957a347b86c0052735
kkaiochristaldo <kkaiochristaldo@gmail.com>
2023-10-10 13:45:18 -0400

    feat: create component InputSearch

---
+++ web/src/components/InputSearch/index.tsx
@@ -0,0 +1,13 @@
+
+
+export default function InputSearch ()
+{
+   return (
+   <input 
+      type="search"
+      className="w-80 rounded px-4 py-[10px] text-sm not-italic bg--200 font-normal text-slate-600 focus:outline-pink-600" 
+      placeholder="Procurando por algo específico?"
+   />
+   )
+
+}
